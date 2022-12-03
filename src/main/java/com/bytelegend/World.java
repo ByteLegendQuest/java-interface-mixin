e7f39d12f2fa1bab5f507d0c6ae1659043b4a4cd
jenny-2233 <822204051@qq.com>
2022-12-03 02:15:54 +0000

    Update at 2022-12-03T02:15:54.789038Z

--- src/main/java/com/bytelegend/World.java
+++ src/main/java/com/bytelegend/World.java
@@ -1,5 +1,7 @@
 package com.bytelegend;
 
+import java.lang.reflect.Field;
+
 public class World {
     /**
      * `Cat` has a `walk()` method. `Fish` has a `swim()` method. Now a `Turtle` wants to reuse
@@ -7,65 +9,90 @@ public class World {
      * this problem? This kind of requirement in object-oriented system is called `mixin`. It can be
      * solved by interface default method.
      */
-    public static void main(String[] args) {
-        new Cat("Tom").walk();
-        new Fish("Nemo").swim();
-        new Turtle("Turtle").walk();
-        new Turtle("Turtle").swim();
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+//        new Cat("Tom").walk();
+//        new Fish("Nemo").swim();
+//        new Turtle("Turtle").walk();
+//        new Turtle("Turtle").swim();
+
+        Cat cat = new Cat("T");
+        Field[] fields = Cat.class.getFields();
+        for (int i = 0; i < fields.length; i++) {
+            System.out.println(fields[i].get(cat));
+        }
+
+
+
     }
+
 }
 
-abstract class WalkingAnimal {
-    private final String name;
+//abstract class WalkingAnimal {
+//    private final String name;
+//
+//    public WalkingAnimal(String name) {
+//        this.name = name;
+//    }
+//
+//    public void walk() {
+//        System.out.println(name + " is walking");
+//    }
+//}
 
-    public WalkingAnimal(String name) {
-        this.name = name;
-    }
+//abstract class SwimmingAnimal {
+//    private final String name;
+//
+//    public SwimmingAnimal(String name) {
+//        this.name = name;
+//    }
+//
+//    public void swim() {
+//        System.out.println(name + " is swimming");
+//    }
+//}
 
-    public void walk() {
-        System.out.println(name + " is walking");
-    }
-}
-
-abstract class SwimmingAnimal {
-    private final String name;
-
-    public SwimmingAnimal(String name) {
-        this.name = name;
-    }
-
-    public void swim() {
-        System.out.println(name + " is swimming");
-    }
-}
-
-class Cat extends WalkingAnimal {
+class Cat extends Animal implements Walking {
     public Cat(String name) {
         super(name);
+        System.out.print(name);
     }
+
+
 }
 
-class Fish extends SwimmingAnimal {
+class Fish extends Animal implements Swimming {
     public Fish(String name) {
         super(name);
+        System.out.print(name);
     }
+
+
 }
 
-/*
+
 interface Swimming {
-    default void swim();
+    default void swim() {
+        System.out.println(" is swimming");
+    }
 }
 interface Walking {
-    default void walk();
+    default void walk() {
+        System.out.println(" is walking");
+    }
 }
 abstract class Animal {
-    private final String name;
+    public final String name;
     public Animal(String name) {
         this.name = name;
     }
 }
 
 class Turtle extends Animal implements Swimming, Walking {
+
+
+    public Turtle(String name) {
+        super(name);
+        System.out.print(name);
+    }
 }
-*/
- 
+
